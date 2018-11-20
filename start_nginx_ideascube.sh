3993158b8390a22fc77b1575f05c62f430b2c21f
swbsf <steven.wallimann@bibliosansfrontieres.org>
2018-11-20 13:42:09 +0100

    Handle nginx through ansible.

--- start_nginx_ideascube.sh
+++ start_nginx_ideascube.sh
@@ -2,5 +2,4 @@
 
 /usr/local/bin/ansible-pull -i hosts -U https://github.com/ideascube/ansiblecubepp.git main.yml --extra-vars "generic_project_name=$PROJECT_NAME"
 
-/etc/init.d/nginx start
 ideascube runserver 8000
