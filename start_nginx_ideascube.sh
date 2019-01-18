96d7597c2599bb7331aff5b044090b005ecaaa08
swbsf <steven.wallimann@bibliosansfrontieres.org>
2019-01-18 16:06:37 +0100

    Making sure library.xml exists, but at the right place.

--- start_nginx_ideascube.sh
+++ start_nginx_ideascube.sh
@@ -4,9 +4,6 @@ export DOMAIN="$PROJECT_NAME.ideascube.docker.ideas-box.eu"
 
 ideascube migrate --run-syncdb
 
-touch /var/ideascube/kiwix/library.xml
-#nohup /usr/local/bin/kiwix-serve --library --port=8002 /var/ideascube/kiwix/library.xml &
-
 /usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecubepp.git main.yml --extra-vars "generic_project_name=$PROJECT_NAME"
 
 ideascube runserver 8000
