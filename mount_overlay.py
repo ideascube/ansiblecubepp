d80812370e6ca3ea16d40c32b66db77489cf55fe
swbsf <steven.wallimann@bibliosansfrontieres.org>
2019-02-01 11:17:04 +0100

    Fixing differents bugs

--- mount_overlay.py
+++ mount_overlay.py
@@ -1,19 +1,22 @@
+#!/usr/bin/python
+
+import os
 from sys import argv
-from subprocess import Popen
+from subprocess import call
 
 # Expected language list is in the form "fr,es,en,". Make it a real python list
 # Only two arguments are expected. First is list of languages, second is the mount point where they are accessible from.
 langs = argv[1].rstrip(",").split(",")
 source = argv[2].rstrip("/")
 
+# Mount tmpfs
+call("mount -t tmpfs tmpfs /tmp/overlay", shell=True)
+
 # Make sure all needed folders are there
-folder_list = ["/tmp/overlay", "/tmp/overlay/work", "/tmp/overlay/upper", "/root/.kolibri/content"]
+folder_list = ["/tmp/overlay/work", "/tmp/overlay/upper", "/root/.kolibri/content"]
 for f in folder_list:
     if not os.path.exists(f):
         os.makedirs(f)
-
-# Mount tmpfs
-Popen("mount -t tmpfs tmpfs /tmp/overlay")
 
 # Build overlay command
 upperdir = "/tmp/overlay/upper"
@@ -22,7 +25,7 @@ merged = "/root/.kolibri/content"
 lowerdir = str()
 
 for lang in langs:
-    lowerdir += source + "/" + lang + ":"
+    lowerdir += source + "/" + lang + "/content:"
 
 lowerdir = lowerdir.rstrip(":")
 
@@ -31,4 +34,4 @@ fullcommand = "mount -t overlay overlay -o lowerdir=" + \
               " " + merged
 
 # Mount overlay
-Popen(fullcommand)
+call(fullcommand, shell=True)
