c64b476edb2fa5a4787adcfe8d7459c59ea565bf
steven <steven.wallimann@bibliosansfrontieres.org>
2019-02-04 15:06:08 +0100

    IDC specificity. Needs  env var.

--- start_nginx_ideascube.sh
+++ start_nginx_ideascube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export IDEASCUBE_ID=idb
-export FULLDOMAIN="$PROJECT_NAME.$PROCESS_ID.ideascube.$DOCKER_DOMAIN"
+export DOMAIN="$PROJECT_NAME.$PROCESS_ID.ideascube.$DOCKER_DOMAIN"
 export KOLIBRI_DOMAIN="$PROJECT_NAME.$PROCESS_ID.ideascube"
 
 mv /var_ideascube/* /var/ideascube/
@@ -9,7 +9,7 @@ echo "DOMAIN=$FULLDOMAIN" >> /etc/default/ideascube
 
 ideascube migrate --run-syncdb
 
-/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecubepp.git main.yml --extra-vars "generic_project_name=$PROJECT_NAME full_domain_name=$FULLDOMAIN env=$ENVIRON kolibri_domain=$KOLIBRI_DOMAIN"
+/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecubepp.git main.yml --extra-vars "generic_project_name=$PROJECT_NAME full_domain_name=$DOMAIN env=$ENVIRON kolibri_domain=$KOLIBRI_DOMAIN"
 
 ideascube runserver 8000
 
