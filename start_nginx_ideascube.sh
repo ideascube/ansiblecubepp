508540e82e6b368b6358cbace9c5727182da71d5
steven <steven.wallimann@bibliosansfrontieres.org>
2019-02-04 09:12:50 +0100

    environment is an Ansible reserved var name.

--- start_nginx_ideascube.sh
+++ start_nginx_ideascube.sh
@@ -6,7 +6,7 @@ mv /var_ideascube/* /var/ideascube/
 
 ideascube migrate --run-syncdb
 
-/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecubepp.git main.yml --extra-vars "generic_project_name=$PROJECT_NAME full_domain_name=$FULLDOMAIN environment=$ENVIRON"
+/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecubepp.git main.yml --extra-vars "generic_project_name=$PROJECT_NAME full_domain_name=$FULLDOMAIN env=$ENVIRON"
 
 ideascube runserver 8000
 
