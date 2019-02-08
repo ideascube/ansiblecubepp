d173325415998a5438f46e3ad16c88e6a5b35589
swbsf <steven.wallimann@bibliosansfrontieres.org>
2019-02-08 10:48:35 +0100

    Adding debugging vars

--- start_nginx_ideascube.sh
+++ start_nginx_ideascube.sh
@@ -7,6 +7,12 @@ mv /var_ideascube/* /var/ideascube/
 
 echo "DOMAIN=$DOMAIN" >> /etc/default/ideascube
 
+# Debug vars :
+echo "env=$ENVIRON"
+echo "DOMAIN=$DOMAIN"
+echo "KOLIBRI_DOMAIN=$KOLIBRI_DOMAIN"
+echo "PROJECT_NAME=$PROJECT_NAME"
+
 ideascube migrate --run-syncdb
 
 /usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecubepp.git main.yml --extra-vars "generic_project_name=$PROJECT_NAME full_domain_name=$DOMAIN env=$ENVIRON kolibri_domain=$KOLIBRI_DOMAIN"
