95bb859c3b7e9798316d3fc5e3a4cb54d7d1b40c
swbsf <steven.wallimann@bibliosansfrontieres.org>
2019-10-10 14:19:26 +0200

    Removing all theses spaces, makes script less pretty, but better logs. One has to choose.

--- start_nginx_ideascube.sh
+++ start_nginx_ideascube.sh
@@ -19,16 +19,18 @@ echo "BRANCH"=$BRANCH
 
 ideascube migrate --run-syncdb
 
-/usr/local/bin/ansible-pull -d /var/lib/ansible/local \
-                            -i hosts \
-                            -C $BRANCH \
-                            -U https://github.com/ideascube/ansiblecubepp.git main.yml \
-                            --extra-vars "generic_project_name=$PROJECT_NAME \
-                                          full_domain_name=$DOMAIN \
-                                          env=$ENVIRON \
-                                          kolibri_domain=$KOLIBRI_DOMAIN \
-                                          deployable=$DEPLOYABLE \
-                                          project_id=$PROJECT_ID"
+/usr/local/bin/ansible-pull \
+-d /var/lib/ansible/local \
+-i hosts \
+-C $BRANCH \
+-U https://github.com/ideascube/ansiblecubepp.git main.yml \
+--extra-vars \
+"generic_project_name=$PROJECT_NAME \
+full_domain_name=$DOMAIN \
+env=$ENVIRON \
+kolibri_domain=$KOLIBRI_DOMAIN \
+deployable=$DEPLOYABLE \
+project_id=$PROJECT_ID"
 
 ideascube runserver 8000
 
